deefae70c65e9622269f37ef8c4ca97731c209ea
Akash <hafejmohammadakash@gmail.com>
2025-06-13 17:42:04 +0600

    fav-icon or title name setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,20 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#E58411',
+        'secondary': '#1E1E1E',
+        'secondary-bg': "#F7F7F7",
+       
+      }
+    },
   },
   plugins: [],
 }
+
