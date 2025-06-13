1f2807cf262ec0e0aeee8cc894643b3b5dc16d51
Akash <hafejmohammadakash@gmail.com>
2025-06-13 23:55:17 +0600

    whay chhose us

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Hero from './Hero'
+import WhayChoose from './WhayChoose'
 
 const Home = () => {
   return (
-    
-    <Hero />
+    <>
+      <Hero />
+      
+      <WhayChoose />
+    </>
   )
 }
 
